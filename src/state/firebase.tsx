2c28c7ef7379dfab5b0eaffced643f95b6b02611
jp0624 <jp0624@gmail.com>
2023-12-12 14:11:25 -0500

    Firebase added

---
+++ src/state/firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_projectId,
+	storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+	appId: import.meta.env.VITE_FIREBASE_appId,
+}
+
+const app = initializeApp(firebaseConfig)
